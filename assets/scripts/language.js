40238055dd59cf543be7b02cbb4d3b3227451eb5
Bruno Henrique leite do nascimento <108976077+DEVBrunohenrique@users.noreply.github.com>
2023-10-19 01:59:57 -0300

    b

---
+++ assets/scripts/language.js
@@ -0,0 +1,11 @@
+function lan(){
+    let div = document.getElementById("container-lan");
+    
+    if(div.style.display === "none"){
+       div.style.display = "block"
+    }
+    else{
+       div.style.display = "none"
+    }
+    
+}
